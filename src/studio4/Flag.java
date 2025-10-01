e9458d828256266035033688e94117f4323c64fe
Braden <b.a.hopkins@wustl.edu>
2025-10-01 15:41:07 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,26 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(255, 255, 0);
+		double[] x = {0.5, 0.42, 0.5};
+   		double[] y = {0.5, 0.525, 0.55};
+   		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenColor(0,104,71);
+		StdDraw.filledRectangle(0.1,0.5,0.225,0.3);
+		StdDraw.setPenColor(206, 17, 38);
+		StdDraw.filledRectangle(0.9,0.5,0.225,0.3);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.3);
+		StdDraw.setPenColor(150, 75, 0);
+		StdDraw.filledCircle(0.5, 0.525, 0.05);
+		StdDraw.setPenColor(61, 130, 136);
+		StdDraw.setPenRadius(0.015);
+		StdDraw.arc(0.5, 0.54, 0.08, 200, 290);
+		StdDraw.setPenColor(129, 134, 81);
+		StdDraw.setPenRadius(0.015);
+		StdDraw.arc(0.5, 0.53, 0.1, 185, 355);
+		StdDraw.setPenColor(161, 160, 114);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.line(0.42, 0.525, 0.42, 0.5);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,20 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double width = in.nextDouble();
+		double height = in.nextDouble();
+		StdDraw.setPenColor(red, green, blue);
+		if (filled == true) {
+			StdDraw.filledRectangle(x, y, width, height);
+		} else {
+			StdDraw.rectangle(x, y, width, height);
+		}
 	}
 }
